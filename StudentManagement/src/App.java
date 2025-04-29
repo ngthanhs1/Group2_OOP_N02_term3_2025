3adae49298d2df363967630e1a6ebd67c5ed5c3d
ngthanhs1 <thanhtuyen191205@gmail.com>
2025-04-29 14:02:12 +0000

    update

--- StudentManagement/src/App.java
+++ StudentManagement/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Group 2 - Student Management");
+        System.out.println("xinchao mn");
         
     }
 }
