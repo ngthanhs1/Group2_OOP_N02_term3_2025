3554cac9c36104e0b3671cf0f51e1e4c8526bf22
Subin1707 <manhquyenxyz@gmail.com>
2025-04-29 04:32:51 +0000

    update

--- StudentManagement/src/App.java
+++ StudentManagement/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("test!");
+        System.out.println("Student Management");
         
     }
 }
