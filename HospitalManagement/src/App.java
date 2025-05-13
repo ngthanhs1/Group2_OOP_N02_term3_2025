b499f2b167a422fc4661bad545f36bac6943abd0
Subin1707 <manhquyenxyz@gmail.com>
2025-05-13 04:45:18 +0000

    dshjgf

--- HospitalManagement/src/App.java
+++ HospitalManagement/src/App.java
@@ -5,6 +5,8 @@ public class App {
     System.out.println("hi");
     System.out.println();
     Patient sc = new Patient(1, "Thanh", 20, "Nam", "Ha Noi", "0987654321");
+    Patient sr = new Patient(1, "Quyen", 20, "Nam", "Ha Noi", "0358309009");
     System.out.println("Name:"+sc.getName());
+    System.out.println("Name:"+sr.getName());
     }
 }
