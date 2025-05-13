21f136840087be5da03a5e796a051a5dbe95dea6
ngthanhs1 <thanhtuyen191205@gmail.com>
2025-05-13 04:01:35 +0000

    new

--- HospitalManagement/src/App.java
+++ HospitalManagement/src/App.java
@@ -7,5 +7,6 @@ public class App {
         System.out.println();
         Patient sc = new Patient(1, "Thanh", 20, "Nam", "Ha Noi", "0987654321");
         System.out.println(sc.id +" "+sc.name+" "+sc.age);
+        System.out.println("Phone: "+sc.getPhoneNumber());
     }
 }
