d3bbaf5869a3acc0b356e1c5add0ea19c09adf92
Subin1707 <manhquyenxyz@gmail.com>
2025-05-13 03:37:44 +0000

    delete studentManagement

--- StudentManagement/src/App.java
+++
@@ -1,9 +0,0 @@
-import StudentManagement.test.testRannumber;
-
-public class App {
-    public static void main(String[] args) throws Exception{
-        testRannumber sr = new testRannumber();
-        int Number = sr.gettestRannumber(1,1000);
-        System.out.println("So ngau nhien: "+Number);
-    }
-}
