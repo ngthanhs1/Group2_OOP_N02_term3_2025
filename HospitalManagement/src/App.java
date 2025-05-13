e4f26b8ec81b2ce63647e045a444767445f5b93c
ngthanhs1 <thanhtuyen191205@gmail.com>
2025-05-13 03:39:16 +0000

    new update

--- HospitalManagement/src/App.java
+++ HospitalManagement/src/App.java
@@ -3,9 +3,9 @@ import HospitalManagement.test.TestRandNumber;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("hi");
+        
         System.out.println();
-        TestRandNumber sr = new TestRandNumber();
-        int number = sr.getTestRandNumber(50, 100);
-        System.out.println("So ngau nhien: "+number);
+        Patient sc = new Patient(1, "Thanh", 20, "Nam", "Ha Noi", "0987654321");
+        System.out.println(sc.getId()+" "+sc.getName()+" "+sc.getAddress());
     }
 }
