abca8797f1e4cc813d28c20fe48f8e5b64e004de
ngthanhs1 <thanhtuyen191205@gmail.com>
2025-06-14 14:29:47 +0000

    new chuyen doi

--- HospitalManagement/src/App.java
+++ HospitalManagement/src/App.java
@@ -1,21 +1,8 @@
 package HospitalManagement.src;
 
-import HospitalManagement.Test.TestBenhan;
-import HospitalManagement.Test.TestPatient;
-import HospitalManagement.Test.TestSchedule;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
-    System.out.println("Chao ca nha iu :3");
-    System.out.println();
-
-
-    TestPatient tpt = new TestPatient();
-    tpt.dsMauPatient();
-    tpt.inPatient();
-    tpt.timKiemPatient();
-    TestBenhan tba = new TestBenhan();
-    tba.themBenhan();
-    tba.inBenhan();
   }
 }
