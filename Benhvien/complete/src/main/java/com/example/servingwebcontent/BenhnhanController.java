01f18f09df0cc68f09fbe9173ab2c490dac0786d
Subin1707 <manhquyenxyz@gmail.com>
2025-06-21 08:32:35 +0000

    cap nhat

---
+++ Benhvien/complete/src/main/java/com/example/servingwebcontent/BenhnhanController.java
@@ -0,0 +1,23 @@
+package com.example.servingwebcontent;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+    @Controller
+public class BenhnhanController {
+
+
+
+
+ //   private final benhanAiven benhanDb = new benhanAiven();
+
+    @GetMapping("/benhan")
+    public String getBenhAn(Model model) {
+       // List<BenhAn> benhans = benhanDb.getBenhanList();
+     ///   model.addAttribute("benhanList", benhans);
+        return "benhan"; // Trả về file templates/benhan.html
+    }
+}
+    
